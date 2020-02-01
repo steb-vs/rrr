4c26acea3972cf66ce50252ff82d315287e5e55b
Steb <steb.vs@outlook.com>
2020-02-01 16:14:09 +0100

    object thrower

---
+++ Unity/RRR/Assets/Scripts/Piece/PieceComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceComponent : MonoBehaviour
+{
+    private PieceParametersComponent _parameters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _parameters = GetComponent<PieceParametersComponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity/RRR/Assets/Scripts/Piece/PieceParametersComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceParametersComponent : MonoBehaviour
+{
+    public PieceColor color;
+    public PieceBrand brand;
+    public PieceShape shape;
+    public PieceSize size;
+    public Vector2 direction;
+    public float velocity;
+
+    public void Initialize(PieceParameters parameters)
+    {
+        color = parameters.Properties.Color;
+        brand = parameters.Properties.Brand;
+        shape = parameters.Properties.Shape;
+        size = parameters.Properties.Size;
+        direction = parameters.Direction;
+        velocity = parameters.Velocity;
+    }
+}
