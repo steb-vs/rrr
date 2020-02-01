df6fb43a46ce2485f4cc6ff2cfc349d36c77f4a4
Martin SCHNEIDER <martin.schneider01@gmail.com>
2020-02-01 16:33:08 +0100

    Player Movement

---
+++ Unity/RRR/Assets/Scripts/PickUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public GameObject item;
+    public GameObject tempParent;
+    public Transform guide;
+
+    void Start()
+    {
+            GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    void OnMouseDown()
+    {
+      // GetComponent<BoxCollider>().enabled = false;
+      item.GetComponent<Rigidbody>().useGravity = false;
+      item.GetComponent<Rigidbody>().isKinematic = true;
+      item.transform.position = guide.transform.position;
+      item.transform.rotation = guide.transform.rotation;
+      item.transform.parent = tempParent.transform;
+    }
+
+    void OnMouseUp()
+    {
+      this.transform.parent = null;
+            item.GetComponent<Rigidbody>().useGravity = true;
+            item.GetComponent<Rigidbody>().isKinematic = false;
+            item.transform.parent = null;
+            item.transform.position = guide.transform.position;
+    }
+}
