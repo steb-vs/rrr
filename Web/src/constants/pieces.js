a00f61ebe0536fb2eae3ba10833f046cbc642a78
YadPe <yannis.petitot@link-value.fr>
2020-02-01 17:58:52 +0100

    add pieces constant

---
+++ Web/src/constants/pieces.js
@@ -0,0 +1,17 @@
+export default {
+  shapes: {
+    square: "square",
+    triangle: "triangle",
+    circle: "circle"
+  },
+  colors: {
+    red: "#fa2323",
+    blue: "#0011c9",
+    green: "#07c900"
+  },
+  brands: {
+    durex: "durexBase64",
+    lays: "laysBase64",
+    ikea: "ikeaBase64"
+  }
+};
