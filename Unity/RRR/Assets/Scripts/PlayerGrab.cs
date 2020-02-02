a3b32287c19d9d20a2e5d082b3b3d585ef73e41a
Martin SCHNEIDER <daydreel@gmail.com>
2020-02-02 12:08:29 +0100

    Grab works very well !

---
+++ Unity/RRR/Assets/Scripts/PlayerGrab.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrab : MonoBehaviour
+{
+    public Camera cam;
+
+
+    public float distance = 100;
+    public Transform destination;
+
+    GameObject pieceComp;
+
+    bool grabbing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(cam.transform.position, cam.transform.forward, Color.green);
+
+        if (pieceComp && grabbing && Input.GetButtonDown("Grab"))
+        {
+            releaseGrab();
+        }
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, distance))
+        {
+
+            if (pieceComp && grabbing)
+            {
+                Grab();
+            }
+
+            if (hit.collider.gameObject.tag == "pickable" && Input.GetButtonDown("Grab") && !grabbing)
+            {
+                pieceComp = hit.collider.gameObject;
+                //pieceComp.GetComponent<Collider>().enabled = false;
+                grabbing = true;
+                Debug.Log("Grabbing");
+            }
+        }
+    }
+
+    void Grab()
+    {
+        pieceComp.transform.SetParent(destination);
+        pieceComp.GetComponent<Rigidbody>().isKinematic = true;
+        pieceComp.GetComponent<Rigidbody>().useGravity = false;
+        pieceComp.GetComponent<BoxCollider>().isTrigger = true;
+        pieceComp.transform.localPosition = Vector3.zero;
+        Debug.Log("Grab");
+    }
+
+    void releaseGrab()
+    {
+        //pieceComp.GetComponent<Collider>().enabled = true;
+        pieceComp.GetComponent<Rigidbody>().useGravity = true;
+        pieceComp.GetComponent<Rigidbody>().isKinematic = false;
+        pieceComp.GetComponent<BoxCollider>().isTrigger = false;
+        pieceComp.transform.SetParent(null);
+        pieceComp = null;
+        grabbing = false;
+        Debug.Log("Release");
+    }
+}
